3da474c9d95b3c809314ac8926459cf2dc41d276
Tomoya Miyara <e165709@ie.u-ryukyu.ac.jp>
2016-12-14 16:20:50 +0900

    step1-version1

---
+++ src/main/java/jp/ac/uryukyu/ie/e165709/Enemy.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e165709;
+
+public class Enemy extends LivingThing{
+
+    public Enemy (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/e165709/Hero.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e165709;
+
+public class Hero extends LivingThing{
+
+    public Hero (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+}
